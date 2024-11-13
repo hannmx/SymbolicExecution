07575cbb96f670196831e8f6c12e341777568c8b
mxkud <mxkudk@yandex.ru>
2024-11-13 16:17:42 +0300

    Init project

---
+++ src/main/java/ru/symbolexec/SymbolicExec/SymbolicExecApplication.java
@@ -0,0 +1,13 @@
+package ru.symbolexec.SymbolicExec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolicExecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbolicExecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/symbolexec/SymbolicExec/SymbolicExecApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.symbolexec.SymbolicExec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbolicExecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
